7721eb4877d43cfa2a652301b03082f0c5999c44
Juan Cruz Ferreira Monteiro <juancruzferreiramonteiro04@gmail.com>
2023-12-17 21:35:18 -0300

    Se agregan cambios a la habilidad Poisson

--- src/entregable/ataques/Poisson.java
+++ src/entregable/ataques/Poisson.java
@@ -1,15 +1,22 @@
 package entregable.ataques;
 
+
+import game.attacks.Sword;
 import game.components.Monster;
+import game.types.Type;
 
 public class Poisson implements Plant{
 
   @Override
   public int damage(Monster monster){
-    if (monster.getLife() >= 600){
+    if (monster.getTypes().contains(Type.SWORD)) {
+      //Si porta una espada es un ser vivo y el veneno le afecta.
+       return monster.getLife() - 300;
+    } else if (monster.getLife() >= 600){
       int damage = monster.getLife()/2;
       return damage;
-    }
+    }  
+
     return 150;
 }
 
